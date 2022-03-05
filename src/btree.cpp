a51c143d9996fbc216c4b54d0153e0142e639640
pat-loughran <71353422+pat-loughran@users.noreply.github.com>
2022-03-05 16:41:07 -0600

    Started insert

--- src/btree.cpp
+++ src/btree.cpp
@@ -77,6 +77,110 @@ void BTreeIndex::handleNew(std::string indexName, BufMgr *bufMgrIn, const int _a
         bufMgr->unPinPage(file, rootPageNo, true);
 }
 
+void BTreeIndex::createFirstChild(int keyInt, RecordId rid, NonLeafNodeInt* root)
+{
+    //create first child page manually
+    Page* firstPage;
+    PageId firstPageId;
+    bufMgr->allocPage(file, firstPageId, firstPage);
+    LeafNodeInt* firstNode = reinterpret_cast<LeafNodeInt*>(firstPage);
+
+    //initalize leaf node values
+    
+
+    // set first entry of child page and unpin
+    firstNode->keyArray[0] = keyInt;
+    firstNode->ridArray[0] = rid;
+    firstNode->rightSibPageNo = 0; // initalize to invalid page number
+    bufMgr->unPinPage(file, firstPageId, true);
+
+    // update root and unpin
+    root->keyArray[0] = keyInt + 1;
+    root->pageNoArray[0] = firstPageId;
+    bufMgr->unPinPage(file, rootPageNum, true); //unpin root
+
+    // update numPages in file and instance
+    numPages++;
+    Page* metaPage;
+    bufMgr->readPage(file, (PageId)1, metaPage);
+    IndexMetaInfo* metaInfo = reinterpret_cast<IndexMetaInfo*>(metaPage);
+    metaInfo->numPages++;
+    bufMgr->unPinPage(file, (PageId)1, true);
+}
+
+int findInsertIndex(int keyInt, LeafNodeInt* curNode)
+{
+    for (int i = 0 ; i < INTARRAYLEAFSIZE; i++) {
+        // leaf is full, subsequent code will split and handle
+        if (curNode->keyArray[INTARRAYLEAFSIZE -1] != INT_MAX) {
+            return INTARRAYLEAFSIZE;
+        }
+
+        int curKey = curNode->keyArray[i];
+        int nextKey = curNode->keyArray[i+1];
+
+        // found empty slot
+        if (curKey == INT_MAX) {
+            return i;
+        }
+        // key goes in first element
+        if (i == 0 && keyInt < curKey) {
+            return 0;
+        }
+
+        // key goes inbetween current and next
+        if (keyInt > curKey && keyInt < nextKey) {
+            return i+1;
+        }
+    }
+}
+
+void BTreeIndex::insertHelper(int index, int keyInt, RecordId rid, NonLeafNodeInt* root, LeafNodeInt* firstNode)
+{
+    int rootValue = INT_MAX;
+    // key is going to be furthest in page
+    if (firstNode->keyArray[index] == INT_MAX) {
+        //insert key and rid
+        firstNode->keyArray[index] = keyInt;
+        firstNode->ridArray[index] = rid;
+        rootValue = keyInt;
+    }
+    else {
+        for (int i = INTARRAYLEAFSIZE-1; i >= 0; i--) {
+            if (firstNode->keyArray[i] != INT_MAX) {
+                firstNode->keyArray[i+1] = firstNode->keyArray[i];
+                firstNode->ridArray[i+1] = firstNode->ridArray[i];
+                if (rootValue == INT_MAX) {
+                    rootValue = firstNode->keyArray[i+1];
+                }
+            }
+        }
+        firstNode->keyArray[index] = keyInt;
+        firstNode->ridArray[index] = rid;
+    }
+    //update root key
+    root->keyArray[0] = rootValue;
+    
+    //unpin root and firstPage
+    bufMgr->unPinPage(file, (PageId)2, true);
+    bufMgr->unPinPage(file, (PageId)3, true);
+
+}
+
+bool BTreeIndex::insertInFirstPage(int keyInt, RecordId rid, NonLeafNodeInt* root)
+{
+    Page* firstPage;
+    bufMgr->readPage(file, (PageId)3, firstPage);
+    LeafNodeInt* firstNode = reinterpret_cast<LeafNodeInt*>(firstPage);
+    int insertIndex = findInsertIndex(keyInt, firstNode);
+    if (insertIndex == INTARRAYLEAFSIZE) {
+        return false;
+    }
+    insertHelper(insertIndex, keyInt, rid, root, firstNode);
+    return true;
+
+}
+
 // -----------------------------------------------------------------------------
 // BTreeIndex::setAttributes -- private helper
 // -----------------------------------------------------------------------------
@@ -95,6 +199,11 @@ void initalizeNonLeafNode(NonLeafNodeInt* nonLeafNode) {
                 nonLeafNode->keyArray[i] = INT_MAX; // pre fill values
             }
             nonLeafNode->pageNoArray[i] = 0; // pre fill values
+        }
+}
+void initalizeLeafNode(LeafNodeInt* leafNode) {
+    for (int i = 0; i <INTARRAYNONLEAFSIZE; i++) {
+        leafNode->keyArray[i] = INT_MAX; // pre fill values
         }
 }
 
@@ -165,7 +274,36 @@ BTreeIndex::~BTreeIndex()
 
 void BTreeIndex::insertEntry(const void *key, const RecordId rid) 
 {
+    int keyInt = *((int*)key);
 
+    // get root page
+    // MUST unpin root when done
+    Page* rootPage;
+    bufMgr->readPage(file, rootPageNum, rootPage);
+    NonLeafNodeInt* root = reinterpret_cast<NonLeafNodeInt*>(rootPage);
+
+    // check if this is the first entry, if so, need to create roots first child manually
+    if (root->pageNoArray[0] == 0) {
+        createFirstChild(keyInt, rid, root);
+        return;
+    }
+
+    // check if more than one child page exists
+    if (numPages == 3) {
+        bool keyInserted = insertInFirstPage(keyInt, rid, root);
+        if (keyInserted) {
+            return;
+        }
+    }
+
+    // at this point, we have a regualr B+ tree where the root node is always a nonleaf node
+    // If this is the first time we are reaching this part of execution, then the root Pages first child is full
+    // So we have                   (root)
+    //                              /
+    //                        (LeafNode)
+    // The next Key/Rid pair to be inserted will cause a split that needs to be handled below among any other
+    // regular insert logic
+    //
 }
 
 // -----------------------------------------------------------------------------
